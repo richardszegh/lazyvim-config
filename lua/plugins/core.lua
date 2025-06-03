85d0c3a00f642b20388097727e64e907e44e388c
Richard Szegh <richard@richardszegh.com>
2025-06-03 13:13:27 +0200

    use prettier for js,ts etc.

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -2,7 +2,7 @@ return {
   {
     "folke/tokyonight.nvim",
     opts = {
-      style = "storm"
+      style = "storm",
     },
   },
 
@@ -19,5 +19,22 @@ return {
         },
       },
     },
-  }
+  },
+
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        javascript = { "prettier" },
+        typescript = { "prettier" },
+        javascriptreact = { "prettier" },
+        typescriptreact = { "prettier" },
+        html = { "prettier" },
+        css = { "prettier" },
+        json = { "prettier" },
+        ["jsonc"] = { "prettier" },
+        markdown = { "prettier" },
+      },
+    },
+  },
 }
