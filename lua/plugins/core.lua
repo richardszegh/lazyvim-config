1b990e4105bce093d2a6bf684aa2042602c4d103
Richard Szegh <richard@richardszegh.com>
2025-09-21 12:37:44 +0200

    updates

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -56,7 +56,6 @@ return {
       { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
     },
     lazy = false,
-    branch = "regexp", -- This is the regexp branch, use this for the new version
     keys = {
       { ",v", "<cmd>VenvSelect<cr>" },
     },
@@ -66,7 +65,7 @@ return {
   },
 
   {
-    "williamboman/mason.nvim",
+    "mason-org/mason.nvim",
     opts = {
       ensure_installed = {
         "typescript-language-server",
